081145b3f832874bbe5082687d4b418fffc5bd25
Evhenii Onyshchuk <zonix300@gmail.com>
2024-01-08 17:21:21 +0100

    Adding some realisation of chat by Web Sockets

--- src/main/java/zonix/chat/component/WebSocketEventListener.java
+++ src/main/java/zonix/chat/component/WebSocketEventListener.java
@@ -1,16 +1,33 @@
 package zonix.chat.component;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import zonix.chat.entity.Message;
+import zonix.chat.entity.MessageType;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent disconnectEvent) {
-        //TODO -- Disconnect handler
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(disconnectEvent.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var message = Message.builder()
+                    .messageType(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSendToUser(username, "/chat/private", message);
+        }
     }
 }
--- src/main/java/zonix/chat/controller/ChatController.java
+++ src/main/java/zonix/chat/controller/ChatController.java
@@ -3,20 +3,33 @@ package zonix.chat.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import zonix.chat.entity.Message;
 
 @Controller
+@RequestMapping
 public class ChatController {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
-    @MessageMapping("/private-message")
-    public Message receivePrivateMessage(@Payload Message message) {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/chat/private")
+    public Message sendMessage(@Payload Message message)
+    {
+        return message;
+    }
 
-        simpMessagingTemplate.convertAndSendToUser(message.getReceiver(), "/private", message);
+    @MessageMapping("/chat.addUser")
+    @SendTo("/chat/private")
+    public Message addUser(@Payload Message message,
+                           SimpMessageHeaderAccessor headerAccessor
+    ) {
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
         return message;
     }
 }
